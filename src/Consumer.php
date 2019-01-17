97542ee8690f34ab16346c31b623f11d317f5727
Ayman Mahgoub <ayman.mahgoub1@gmail.com>
2019-01-17 14:34:24 +0200

    try to automate

--- src/Consumer.php
+++ src/Consumer.php
@@ -4,16 +4,16 @@ namespace App;
 
 use App\src\DataSources\DataSourceFactory;
 
-class Consumer
-{
-    private $dataSource;
+class Consumer           {
+    private      $dataSource;
 
     /**
      * Consumer constructor.
      * @param string $dataSource
+     *
      * @param string $path
      */
-    public function __construct(string $dataSource, string $path)
+    public function __construct(string $dataSource  , string $path)
     {
         $this->dataSource =  DataSourceFactory::getDataSourceInstance($dataSource, $path);
     }
